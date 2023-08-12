97830d62dea06901c208c562123ab766a4137a67
chrystal <cltacker13@gmail.com>
2023-08-12 15:36:51 -0400

    update src links and index

---
+++ functions.js
@@ -0,0 +1,6 @@
+// future in app funtions
+
+//placeholder function.
+export function add(a,b) {
+    return a + b
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+//internal app functions - placeholder
+import { add } from "../functions.js"
+//firebase app building functions
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+//app initialization and database set up
+const appSettings = {
+    databaseURL: "https://collections-d0f5c-default-rtbd.firebaseio.com" 
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const gamesInDB = ref(database, "Games")
+
+//input functionality
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const gameListEl = document.getElementById("game-list")
+
+//add entry to database & clear input field
+addButtonEl.addEventListener("click", function() {
+   let inputValue = inputFieldEl.value
+   push(gamesInDB, inputValue)
+   clearInputFieldEl()
+})
+
+//call data  array to loop, clear list, & append new list to display
+onValue(gamesInDB, function(snapshot) {
+    //let gamesArray = Object.entries(snapshot)
+    let gameListArray = Object.values(snapshot.val())
+    clearGameListEl()
+    for (let i = 0; i < gameListArray.length; i++) {
+        let currentGame = gameListArray[i]
+        appendGameToGameListEl(currentGame)
+    }
+})
+
+function clearInputFieldEl() {
+    inputFieldEl = ""
+}
+
+function clearGameListEl() {
+    gameListEl.innerHTML = ""
+}
+
+function appendGameToGameListEl(gameValue) {
+    gameListEl.innerHTML += '<li>${value}</li>'
+}
